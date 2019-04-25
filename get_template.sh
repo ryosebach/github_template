42623a664fc13b5a23202b950785645c02efd91a
ryosebach <ryo.freezia@gmail.com>
2019-04-25 18:10:22 +0900

    🔇 add POST_MORTEM.md to gitignore

--- get_template.sh
+++ get_template.sh
@@ -4,4 +4,6 @@ mkdir .github
 curl -sL raw.github.com/ryosebach/github_template/master/ISSUE_TEMPLATE.md -o .github/ISSUE_TEMPLATE.md
 curl -sL raw.github.com/ryosebach/github_template/master/PULL_REQUEST_TEMPLATE.md -o .github/PULL_REQUEST_TEMPLATE.md
 curl -sL raw.github.com/ryosebach/github_template/master/POST_MORTEM.md -o POST_MORTEM.md
+touch .gitignore
+sed -i '1iPOST_MORTEM.md' .gitignore
 
